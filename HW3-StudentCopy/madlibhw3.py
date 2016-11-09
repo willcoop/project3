45df3eaad35bfc45f0253013e5d915065c862cd7
Will Cooper <willcooper@0587345706.wireless.umich.net>
2016-11-09 17:58:57 -0500

    My third commit. This one is committing the changes from the twitterhw3b.py file for the 4th challenge of project 3.

--- HW3-StudentCopy/madlibhw3.py
+++ HW3-StudentCopy/madlibhw3.py
@@ -9,8 +9,67 @@
 # Deliverables:
 # 1) Print the orginal text (150 tokens)
 # 1) Print the new text
-print("START*******")
 
+print("START*******")
+# code developed by Jackie Cohen; revised by Paul Resnick
+# further revised by Colleen van Lent for Python3
+import nltk # requires some downloading/installing dependencies to use all its features; numpy is especially tricky to install
+from nltk.book import *
+import random
+
+
+# import nltk
+#nltk.download('punkt')
+
+from nltk import word_tokenize,sent_tokenize
+
+debug = False #True
+
+string1 = (text2[:151])
+joint = ' '.join(string1)
+print (joint)
+
+'''# get file from user to make mad lib out of
+if debug:
+	print ("Getting information from file text2.txt...\n")
+fname = "madlibtest2.txt" # need a file with this name in directory
+
+f = open(fname, 'r')
+joint = f.read()'''
+
+tokens = nltk.word_tokenize(joint)
+
+#print("TOKENS")
+#print(tokens)
+
+#tagged_tokens = nltk.pos_tag(tokens) # gives us a tagged list of tuples
+#print("TAGGED TOKENS")
+#print(tagged_tokens)
+#if debug:
+#	print ("First few tagged tokens are:")
+#	for tup in tagged_tokens[:5]:
+#		print (tup)
+
+tagmap = {"NN":"a noun","NNS":"a plural noun","VB":"a verb","JJ":"an adjective","P":"Preposition"}
+substitution_probabilities = {"NN":.15,"NNS":.1,"VB":.1,"JJ":.1, "P":1}
+
+def spaced(word):
+	if word in [",", ".", "?", "!", ":"]:
+		return word
+	else:
+		return " " + word
+
+final_words = []
+
+
+for (word, tag) in tagged_tokens:
+	if tag not in substitution_probabilities or random.random() > substitution_probabilities[tag]:
+		final_words.append(spaced(word))
+	else:
+		new_word = input("Please enter %s:\n" % (tagmap[tag]))
+		final_words.append(spaced(new_word))
+
+print ("".join(final_words))
 
 print("\n\nEND*******")
 
--- HW3-StudentCopy/twitterhw3b.py
+++ HW3-StudentCopy/twitterhw3b.py
@@ -7,6 +7,53 @@
 
 # Be prepared to change the search term during demo.
 
+import tweepy
+from textblob import TextBlob
 
-print("Average subjectivity is")
-print("Average polarity is")
+# Unique code from Twitter
+access_token = "461609804-mLrHq4Jx5XE6RRcZDWNPL0sLAfN1T9yFUgMgfSX1"
+access_token_secret = "19TBw847DydvdQRQTwTcUI6q5ZLS6HUvWOwZJy5bQbFp5"
+consumer_key = "y69RWibGPebPJW99m02wzYGXY"
+consumer_secret = "tzKFxQtHYCVOyrnYcHkGrHXggao0PD1bHGdLd0qodmOoJFS2XX"
+
+# Boilerplate code here
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+api = tweepy.API(auth)
+#Now we can Create Tweets, Delete Tweets, and Find Twitter Users
+
+public_tweets = api.search('#Trump')
+
+pol_count = 0
+pol_total = 0
+
+sub_count = 0 
+sub_total = 0
+
+
+for tweet in public_tweets:
+	print(tweet.text)
+	analysis = TextBlob(tweet.text)
+
+
+	pol_count = pol_count + 1
+	pol_total = pol_total + analysis.polarity
+
+	sub_count = sub_count + 1
+	sub_total = sub_total + analysis.subjectivity
+
+
+
+		
+
+
+avg_pol = pol_total/pol_count
+avg_sub = sub_total/sub_count
+
+
+
+
+
+print("Average subjectivity is " + str(avg_sub))
+print("Average polarity is " + str(avg_pol))
