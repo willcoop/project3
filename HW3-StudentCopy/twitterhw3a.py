e426b01ca590b57771b168415d330e32017fdb33
Will Cooper <willcooper@Wills-MacBook-Pro.local>
2016-11-09 13:47:50 -0500

    This commit is commiting my code for challenge 3 on HW 3 on the twitterhw3a.py file

--- HW3-StudentCopy/twitterhw3a.py
+++ HW3-StudentCopy/twitterhw3a.py
@@ -3,6 +3,30 @@
 # hashtags #UMSI-206 #Proj3 in the tweet.
 
 # You will demo this live for grading.
+import tweepy
 
-print("""No output necessary although you 
-	can print out a success/failure message if you want to.""")
+# Unique code from Twitter
+access_token = "461609804-mLrHq4Jx5XE6RRcZDWNPL0sLAfN1T9yFUgMgfSX1"
+access_token_secret = "19TBw847DydvdQRQTwTcUI6q5ZLS6HUvWOwZJy5bQbFp5"
+consumer_key = "y69RWibGPebPJW99m02wzYGXY"
+consumer_secret = "tzKFxQtHYCVOyrnYcHkGrHXggao0PD1bHGdLd0qodmOoJFS2XX"
+
+# Boilerplate code here
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+api = tweepy.API(auth)
+#Now we can Create Tweets, Delete Tweets, and Find Twitter Users
+
+img = "jetsnation.jpg"
+txt = "#UMSI-206 #Proj3"
+
+api.update_with_media(img,status=txt)
+	
+#Learn more about Search
+#https://dev.twitter.com/rest/public/search
+
+
+
+
+print("""No output necessary""")
